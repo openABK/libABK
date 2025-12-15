3f3dfe678b2db25f6242b6419a68c4d16b455259
Patrick Zacharias <1475802+Fighter19@users.noreply.github.com>
2025-12-15 12:57:25 +0100

    Add implementations of DeleteSession and ObtainSessionId back to base class

--- ClientBoost/AbkClientAbstraction.h
+++ ClientBoost/AbkClientAbstraction.h
@@ -5,54 +5,52 @@
 
 namespace Abk
 {
-  class CBaseAbstraction
+class CBaseAbstraction
+{
+protected:
+  class CAbkClient *m_pOwner;
+
+public:
+  CBaseAbstraction(class CAbkClient *pClient);
+  virtual ~CBaseAbstraction();
+
+  virtual bool NavigatePut(LPCTSTR pszPath, int nSessionId, const std::string &strPutData);
+  virtual std::string NavigateGet(LPCTSTR pszPath, int nSessionId);
+  virtual bool NavigateGet(LPCTSTR pszPath, int nSessionId, std::ostream &out);
+  virtual std::string NavigatePost(LPCTSTR pszPath, int nSessionId, const std::string &strPostData);
+  virtual bool NavigateDelete(LPCTSTR pszPath, int nSessionId, const std::string &strDeleteData);
+  virtual unsigned int GetStatus() const;
+
+  virtual void Close();
+  virtual bool EnsureConnection();
+  virtual void SetServerAddr(const std::string &pszServerAddress, int nPort); // re-assigns the server address and port
+
+  // This one is new
+  virtual bool IsConnected() const;
+
+  // TODO: I feel like these don't belong here, they should be in AbkClient.cpp using helper functions.
+  //       For now the implementations have been moved here to the base class, though to avoid having to touch too many uses.
+  bool DeleteSession(int nSessionId);
+  int ObtainSessionId(LPCTSTR pszClientClass, LPCTSTR pszClientType, LPCTSTR pszClientSerial /*=NULL*/, LPCTSTR pszClientFwRev,
+                      LPCTSTR pszClientHwRev);
+
+  // This is just here because eliminating it from here would be a huge PITA
+  template <typename U> bool SetVarOrMailboxValue(LPCTSTR pszPath, const char *pszName, const U *pSet)
   {
-  public:
-    CBaseAbstraction(class CAbkClient *pClient);
-    virtual ~CBaseAbstraction();
-
-    // bool NavigatePut(LPCTSTR pszPath, int nSessionId, CJsonFormatter *pPutData);
-    // std::string NavigateGet(LPCTSTR pszPath, int nSessionId);
-    // bool NavigateGet(LPCTSTR pszPath, int nSessionId, std::ostream &out);
-    // std::string NavigatePost(LPCTSTR pszPath, int nSessionId, CJsonFormatter *pPostData);
-    // bool NavigateDelete(LPCTSTR pszPath, int nSessionId, CJsonFormatter *pPostData);
-
-    bool NavigatePut(LPCTSTR pszPath, int nSessionId, const std::string &strPutData);
-    std::string NavigateGet(LPCTSTR pszPath, int nSessionId);
-    bool NavigateGet(LPCTSTR pszPath, int nSessionId, std::ostream &out);
-    std::string NavigatePost(LPCTSTR pszPath, int nSessionId, const std::string &strPostData);
-    bool NavigateDelete(LPCTSTR pszPath, int nSessionId, const std::string &strDeleteData);
-
-    unsigned int GetStatus() const;
-
-    // This one is new
-    bool IsConnected() const;
-
-    // TODO: I feel like these don't belong here, they should be in AbkClient.cpp
-    bool DeleteSession(int nSessionId); // deletes the actual session
-    int ObtainSessionId(LPCTSTR pszClientClass, LPCTSTR pszClientType, LPCTSTR pszClientSerial /*=NULL*/, LPCTSTR pszClientFwRev, LPCTSTR pszClientHwRev);
-    void Close();
-    bool EnsureConnection();
-    void SetServerAddr(const std::string &pszServerAddress, int nPort); // re-assigns the server address and port
-
-    // This is just here because eliminating it from here would be a huge PITA
-    template <typename U>
-    bool SetVarOrMailboxValue(LPCTSTR pszPath, const char *pszName, const U *pSet)
+    CJsonFormatter jfRequest;
     {
-      CJsonFormatter jfRequest;
+      CJsonStreamArray jaGetList(&jfRequest, ABK_REQ_VARVALUE_PUTLIST);
       {
-        CJsonStreamArray jaGetList(&jfRequest, ABK_REQ_VARVALUE_PUTLIST);
-        {
-          CJsonStreamObject joVarPut(&jaGetList);
-          joVarPut.WriteValue(ABK_REQ_VARVALUE_NAME, pszName);
-          joVarPut.WriteValue(ABK_REQ_VARVALUE_VALUE, *pSet);
-        }
-      } // let array object fall out of scope
-      jfRequest.Close();
-      return NavigatePut(pszPath, -1, jfRequest.GetStream()->str());
-    }
+        CJsonStreamObject joVarPut(&jaGetList);
+        joVarPut.WriteValue(ABK_REQ_VARVALUE_NAME, pszName);
+        joVarPut.WriteValue(ABK_REQ_VARVALUE_VALUE, *pSet);
+      }
+    } // let array object fall out of scope
+    jfRequest.Close();
+    return NavigatePut(pszPath, -1, jfRequest.GetStream()->str());
+  }
 
-    bool GetVarOrMailboxList(LPCTSTR pszPath, std::vector<CString> *pGet);
-    bool GetVarOrMailboxMeta(const std::vector<LPCTSTR> &vectVarNames, std::vector<class CAbkClientMeta> *pGet, bool bMailboxFlag);
-  };
+  bool GetVarOrMailboxList(LPCTSTR pszPath, std::vector<CString> *pGet);
+  bool GetVarOrMailboxMeta(const std::vector<LPCTSTR> &vectVarNames, std::vector<class CAbkClientMeta> *pGet, bool bMailboxFlag);
+};
 } // namespace Abk
--- ClientBoost/AbkClientSoup.cpp
+++ ClientBoost/AbkClientSoup.cpp
@@ -2,6 +2,11 @@
 
 #include <libsoup/soup.h>
 
+#include "JsonParser.h"
+
+// Required to add to log
+#include "AbkClient.h"
+
 namespace Abk
 {
 
@@ -47,12 +52,49 @@ bool CBaseAbstraction::IsConnected() const
 
 bool CBaseAbstraction::DeleteSession(int nSessionId)
 {
-  return false;
+  CJsonFormatter jfSend;
+  return NavigateDelete(_T(ABK_REQUESTURL_SESSIONID), nSessionId, jfSend.GetStream()->str());
 }
 
-int CBaseAbstraction::ObtainSessionId(LPCTSTR pszClientClass, LPCTSTR pszClientType, LPCTSTR pszClientSerial /*=NULL*/, LPCTSTR pszClientFwRev, LPCTSTR pszClientHwRev)
+  /**
+@param pszClientClass class name of the client
+@param pszClientType type name of the client
+@param pszClientSerial serial number or id of the client. If an empty string, the field will not be encoded
+@param pszClientFwRev Firmware revision of the client. If an empty string, the field will not be encoded
+@param pszClientHwRev Hardware revision of the client. If an empty string, the field will not be encoded
+@return session id, -1 on error
+*/
+int CBaseAbstraction::ObtainSessionId(LPCTSTR pszClientClass, LPCTSTR pszClientType, LPCTSTR pszClientSerial /*=NULL*/,
+                                      LPCTSTR pszClientFwRev, LPCTSTR pszClientHwRev)
 {
-  return -1;
+  int nSessionId = -1; // result
+  assert(pszClientClass);
+  assert(pszClientType);
+  CJsonFormatter jfPost;
+  jfPost.WriteValue(ABK_REQ_SESSIONID_CLASS, CT2A(pszClientClass));
+  jfPost.WriteValue(ABK_REQ_SESSIONID_TYPE, CT2A(pszClientType));
+
+  if (pszClientSerial && pszClientSerial[0])
+    jfPost.WriteValue(ABK_REQ_SESSIONID_SERIAL, CT2A(pszClientSerial));
+  if (pszClientFwRev && pszClientFwRev[0])
+    jfPost.WriteValue(ABK_REQ_SESSIONID_FWVERSION, CT2A(pszClientFwRev));
+  if (pszClientHwRev && pszClientHwRev[0])
+    jfPost.WriteValue(ABK_REQ_SESSIONID_HWVERSION, CT2A(pszClientHwRev));
+
+  std::string pReturn = NavigatePost(_T(ABK_REQUESTURL_SESSIONID), -1, jfPost.GetStream()->str());
+  if (pReturn.empty())
+    return -1;
+
+  // extract the session id
+  CJsonParser parsResponse(pReturn.c_str());
+  for (; !parsResponse.IsDone(); ++parsResponse)
+    parsResponse.ExtractValue(ABK_RSP_SESSIONID_ID, &nSessionId);
+  if (nSessionId < 0)
+  {
+    m_pOwner->AddLog(CAbkClient::LOGSEVERITY_ERROR, _T("Got no session id from server."));
+    return -1;
+  }
+  return nSessionId;
 }
 
 void CBaseAbstraction::Close()
